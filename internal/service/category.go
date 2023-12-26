bff847291ed1643ea2bb4f9fd0308c03e3cc4ca9
Jhonatan Henkel <jhowrf@gmail.com>
2023-12-26 15:30:42 -0300

    refactor(category): refatorando retorno response
    Agora o response de inserção de categoria vai retornar apenas os dados da categoria cadastrada, antes vinha em um objeto com a categoria inserida dentro

--- internal/service/category.go
+++ internal/service/category.go
@@ -15,15 +15,14 @@ func NewCategoryService(db database.Category) *CategoryService {
 	return &CategoryService{CategoryDB: db}
 }
 
-func (c *CategoryService) CreateCategory(context context.Context, input *pb.CreateCategoryRequest) (*pb.CategoryResponse, error) {
+func (c *CategoryService) CreateCategory(context context.Context, input *pb.CreateCategoryRequest) (*pb.Category, error) {
 	category, err := c.CategoryDB.Create(input.Name, input.Description)
 	if err != nil {
 		return nil, err
 	}
-	CategoryResponse := &pb.Category{
+	return &pb.Category{
 		Id:          category.ID,
 		Name:        category.Name,
 		Description: category.Description,
-	}
-	return &pb.CategoryResponse{Category: CategoryResponse}, nil
+	}, nil
 }
